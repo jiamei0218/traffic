74edc2ce5541baf3a76cacc52f422ef6a157e3d1
jiamei0218 <1505016783@qq.com>
2021-03-19 23:12:54 +0800

    第二天下午中间多线程

---
+++ app/uis/Video.py
@@ -0,0 +1,52 @@
+#创建一个线程：执行视频流获取操作
+#QThread
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import pyqtSignal
+import cv2 as cv
+#当场线程要有执行的方法已经定义过了是run
+#继承线程类
+#重写run方法：当前线程的功能实现（读取视频流:获取摄像头，循环拿到每一帧数据）
+#创建实例.start（） 执行线程
+
+#线程的数据交互
+#线程数据-----传递给主窗口
+#定义信号（传递的数据类型）
+#发送信号（真实数据）
+#信号指定接受者
+#接受者指定槽函数（数据处理函数）
+#定义槽函数
+
+
+
+class Video(QThread):
+    #定义信号（传递的数据类型）
+    sing_show = pyqtSignal(int,int,int,bytes)
+    def __init__(self):
+        super(Video,self).__init__()
+        #获取摄像头 第0个摄像头
+        #self.dev = cv.VideoCapture(0,cv.CAP_DSHOW)
+
+        #获取本地资源
+        self.dev = cv.VideoCapture("data\\t.mp4",cv.CAP_DSHOW)
+        self.dev.open("data\\t.mp4")
+
+    #重写run方法
+    def run(self):
+        # 循环逐帧捕获数据
+
+        while True:
+            # 一帧数据
+            status, frame = self.dev.read()
+            # frame opencv numpy 数组
+            #数据处理
+            h,w,c = frame.shape
+            deta = frame.tobytes()
+            #发送信号（真实数据）触发发送信号
+            self.sing_show.emit(h,w,c,deta)
+            #等若干时间 0.1秒
+            # usleep(微妙)  1s=1000000微妙
+            QThread.usleep(100000)
+
+
+
+
--- app/uis/trafficframe.py
+++ app/uis/trafficframe.py
@@ -1,9 +1,32 @@
 from PyQt5.QtWidgets import QDialog
-
-from  app.uis.trafficui import Ui_Dialog
+from PyQt5.QtGui import QImage,QPixmap
+from PyQt5.Qt import Qt
+from app.uis.trafficui import Ui_Dialog
+import cv2 as cv
+from app.uis.Video import Video
 
 class TrafficFrame(QDialog):
     def __init__(self):
         super(TrafficFrame,self).__init__() 
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
+        #线程
+        self.th = Video()
+        #信号指定接受者
+        #接受者指定槽函数（数据处理函数）
+        self.th.sing_show.connect(self.show_video)
+
+        #启动线程
+        self.th.start()
+    #定义槽函数
+    def show_video(self,h,w,c,data):
+        image = QImage(data,w,h,w*c,QImage.Format_BGR888)
+        pix = QPixmap.fromImage(image)
+        #width = self.ui.label_9.width()
+        #height = self.ui.label_9.height()
+        #scale_pix = pix.scaled(width,height,Qt.KeepAspectRatio)
+        #self.ui.label_9.setPixmap(scale_pix)不缩放
+        self.ui.label_9.setPixmap(pix)
+        self.ui.label_9.setScaledContents(True)
+
